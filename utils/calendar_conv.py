89df3381b964f6a42fabe66be9d005c00f6d7b61
Judy Yang <57917247+Judyxyang@users.noreply.github.com>
2025-06-30 17:30:45 +1000

    Update calendar_conv.py

--- utils/calendar_conv.py
+++ utils/calendar_conv.py
@@ -5,9 +5,12 @@ from zhdate import ZhDate
 def solar_to_lunar(year, month, day):
     """将阳历日期转换为阴历日期"""
     lunar_date = ZhDate.from_datetime(datetime.datetime(year, month, day))
+    
+    leap_prefix = "闰" if lunar_date.is_leap_month else ""
+    
     return (
         lunar_date.lunar_year,
-        lunar_date.lunar_month,
-        lunar_date.lunar_day,
-        "闰" if lunar_date.isleap else ""
+        leap_prefix + str(lunar_date.lunar_month),
+        lunar_date.lunar_day
     )
+
